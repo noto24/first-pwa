6a79438b50eb591ea02521ccf18e1a6d1c372a88
noto24 <mohamadkarnoto90@gmail.com>
2019-11-01 23:23:25 +0700

    menambahkan beberpa file

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "firstpwa";
+
+var urlsToCache = [
+
+    "/",
+    "/nav.html",
+    "/index.html",
+    "/pages/home.html",
+    "/pages/about.html",
+    "/pages/contact.html",
+    "/css/materialize.min.css",
+    "/js/materialize.min.js",
+    "/js/nav.js"
+];
+
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches
+            .match(event.request, { cacheName: CACHE_NAME })
+            .then(function (response) {
+                if (response) {
+                    console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                    return response;
+                }
+
+                console.log(
+                    "ServiceWorker: Memuat aset dari server: ",
+                    event.request.url
+                );
+                return fetch(event.request);
+            })
+    );
+});
